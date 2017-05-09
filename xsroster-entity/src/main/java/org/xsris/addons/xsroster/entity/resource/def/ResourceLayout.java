a5ebcd64f5d976f5b8d90cddd302d7622730a24e
mythsya <myth.xie@gmail.com>
2017-05-09 17:43:29 +0800

    resource entities

--- xsroster-entity/src/main/java/org/xsris/addons/xsroster/entity/resource/def/ResourceLayout.java
+++ xsroster-entity/src/main/java/org/xsris/addons/xsroster/entity/resource/def/ResourceLayout.java
@@ -1,5 +1,7 @@
 package org.xsris.addons.xsroster.entity.resource.def;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -10,5 +12,10 @@ import org.xsris.addons.xsroster.entity.base.OptimisticLockingTrackableEntity;
 public class ResourceLayout extends OptimisticLockingTrackableEntity {
 
 	private static final long serialVersionUID = 5567141433356656185L;
+
+	private String code;
+	private String title;
+	private List<ResourceDefinition> rowHeaders;
+	private List<ResourceDefinition> colHeaders;
 
 }
